89d2ae2ef6ead03dff6c00aa83a7e8bd54ae8fb8
danbiilee <danbi.db@gmail.com>
2021-02-04 12:35:03 +0900

    [FEAT] #1 : 컬러 변경 debounce 적용

---
+++ src/components/common/Color.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import { debounce } from 'lodash';
+
+const Block = styled.input.attrs(props => ({
+  type: props.type,
+}))`
+  width: 40px;
+  height: 30px;
+  cursor: pointer;
+`;
+
+const Input = ({ onChange, ...props }) => {
+  const { setProperties } = props;
+  // useState의 경우 매번 컴포넌트가 리렌더되서, 디바운스 적용 안됨
+  const color = useRef(props.value);
+
+  const debounceChange = debounce(() => {
+    setProperties(properties => ({
+      ...properties,
+      color: color.current,
+    }));
+  }, 100);
+
+  const handleChange = e => {
+    color.current = e.target.value;
+    debounceChange();
+  };
+
+  return <Block type="color" {...props} onChange={handleChange} />;
+};
+
+export default React.memo(Input);
